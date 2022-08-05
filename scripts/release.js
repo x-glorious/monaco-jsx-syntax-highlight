41499876432786cdfec89cfa0617212a4c956fa4
laoyao <ordinary_person@outlook.com>
2022-08-05 16:44:22 +0800

    fix: adjust release script exec shell timing sequence

--- scripts/release.js
+++ scripts/release.js
@@ -1,4 +1,4 @@
-const ExecSh = require('exec-sh')
+const ExecShPromise = require('exec-sh').promise
 const Minimist = require('minimist')
 
 const Path = require('path')
@@ -8,16 +8,20 @@ const argv = Minimist(process.argv.slice(2))
 // version
 const version = argv['v'].trim()
 
-// update package.json version
-const packageFilePath = Path.join(process.cwd(), './package.json')
-const packageObj = JSON.parse(Fs.readFileSync(packageFilePath).toString())
-packageObj.version = version.toString()
-Fs.writeFileSync(packageFilePath, JSON.stringify(packageObj, null, 2))
+const run = async () => {
+  // update package.json version
+  const packageFilePath = Path.join(process.cwd(), './package.json')
+  const packageObj = JSON.parse(Fs.readFileSync(packageFilePath).toString())
+  packageObj.version = version.toString()
+  Fs.writeFileSync(packageFilePath, JSON.stringify(packageObj, null, 2))
 
-ExecSh('git checkout master')
-ExecSh(`git checkout -b release/v${version}`)
-ExecSh('git add .')
-ExecSh(`git commit --m "chore: public v${version}"`)
-ExecSh(`git push origin release/v${version}`)
-ExecSh(`git tag v${version}`)
-ExecSh(`git push origin v${version}`)
+  await ExecShPromise('git checkout master')
+  await ExecShPromise(`git checkout -b release/v${version}`)
+  await ExecShPromise('git add .')
+  await ExecShPromise(`git commit --m "chore: public v${version}"`)
+  await ExecShPromise(`git push origin release/v${version}`)
+  await ExecShPromise(`git tag v${version}`)
+  await ExecShPromise(`git push origin v${version}`)
+}
+
+run()
